fc006b0b8cf5d3525802e66b0f689192a8dcc89c
MatiMahlknecht <mmahlknecht@pioix.edu.ar>
2025-06-10 16:27:27 -0300

    Hice varias funciones del back
    Coloqué las nuevas funciones que modifican el back

--- back/index.js
+++ back/index.js
@@ -17,12 +17,63 @@ app.get('/', function (req, res) {
     });
 });
 
-app.get('/users', async function (req, res) {
+app.get('/usersRanking', async function (req, res) {
     try {
-        let respuesta = await realizarQuery(`SELECT nombre FROM Usuarios ORDER BY puntaje LIMIT 10`);
+        let respuesta = await realizarQuery(`SELECT nombre, record FROM Usuarios ORDER BY record DESC LIMIT 10`);
         res.send(respuesta);
     }
     catch (error) {
         res.send({ mensaje: "Tuviste un error", error: error.message });
+    }
+})
+
+app.get('/puntaje'), async function (req, res) {
+    try {
+        let respuesta = await realizarQuery(`SELECT record FROM Usuarios ORDER BY record DESC LIMIT 1`);
+        res.send(respuesta);
+    } catch (error) {
+        res.send({ mensaje: "Tuviste un error", error: error.message })
+    }
+}
+
+//Esto es para que lo usen en el front y obtengan el id que le van a agregar a la tbal de puntajes a la columna users
+app.get('/idDeUsuario'), async function (req, res) {
+    try {
+        let respuesta = await realizarQuery(`SELECT id_usuario FROM Usuarios WHERE username = "${req.query.username}"`);
+        res.send(respuesta);
+    } catch (error) {
+        res.send({ mensaje: "Tuviste un error", error: error.message })
+    }
+}
+
+app.post('/users', async function (req, res) {
+    try {
+        let check = await realizarQuery(`SELECT username FROM Usuarios WHERE username = "${req.body.username}"`);
+        if (check !== undefined) {
+            await realizarQuery(`INSERT INTO Usuarios (username, password, record) VALUES
+                ("${req.body.username}", "${req.body.password}", 0)`)
+        } else {
+            res.send("No se pudo agregar el usuario, ya existe otro con ese nombre")
+        };
+    } catch (error) {
+        res.send({ mensaje: "Tuviste un error", error: error.message })
+    }
+})
+
+app.post('/nuevoPuntaje'), async function (req, res) {
+    try {
+        await realizarQuery(`INSERT INTO Puntaje (fecha, puntaje, user) VALUES
+            (${new Date()}, ${req.body.puntaje}, ${req.body.user})`)
+    } catch (error) {
+        res.send({ mensaje: "Tuviste un error", error: error.message })
+    }
+
+}
+
+app.put('/record', async function (req, res) {
+    try {
+        await realizarQuery(`UPDATE Usuarios SET record = ${req.body.record} WHERE id_usuario = ${req.body.id_usuario}`);
+    } catch (error) {
+        res.send({ mensaje: "Tuviste un error", error: error.message })
     }
 })
