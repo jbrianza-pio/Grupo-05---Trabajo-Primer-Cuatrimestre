a5e3cb44f8147a6957f6aadfdd7ae45e8483e753
jbrianza-pio <jmbrianza@pioix.edu.ar>
2025-06-11 10:13:30 -0300

    loop
    planteo de loop
    planteo de comparacion
    falta:
    -reemplazos

--- front/script.js
+++ front/script.js
@@ -8,11 +8,32 @@ function selectRandomPeli(category){
         let number= getRandomInt();
         let selection = fetchPelicula(number, category); //establecer funcion fetch get pelicula. 
         // Parametro dado ID y category a seleccionar.Parametro a espera TITLE, IMAGE_URL Y numero de categoria
-        if (selection.lenght!=3){
+        if (selection.length!=3){
             return selection
         }
         good++
     }
     alert("ERROR 444","Fallo de conexion con base de datos")
     return -1
+}
+function replaceSec1(){
+
+}
+function replaceSec2(){
+
+}
+function loop(category){
+    let peliculaSec1 =selectRandomPeli(category);
+    replaceSec1(); //falta definir variable y UI
+    let peliculaSec2 =selectRandomPeli(category);
+    replaceSec1(); //falta definir variable y UI
+    let game=true
+    let mayor
+    while (game){
+        if (peliculaSec1.category>peliculaSec2.category){//compara cuales mayor
+            mayor=peliculaSec1
+        }else{
+            mayor=peliculaSec1
+        }
+    }
 }
