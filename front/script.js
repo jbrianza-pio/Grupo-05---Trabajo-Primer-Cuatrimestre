17e086b2f4577579c4dd694f8266d21357dea730
jmbrianza <jmbrianza@pioix.edu.ar>
2025-06-11 08:00:18 -0300

    Mejora Loop de Seleccion
    Hice:
    - cambio de seguridad en selectRandomPeli

--- front/script.js
+++ front/script.js
@@ -3,13 +3,16 @@ function getRandomInt(){
         return random
 }
 function selectRandomPeli(category){
-    let good = false;
-    while (good=false){
+    let good = 0;
+    while (good<2){
         let number= getRandomInt();
         let selection = fetchPelicula(number, category); //establecer funcion fetch get pelicula. 
         // Parametro dado ID y category a seleccionar.Parametro a espera TITLE, IMAGE_URL Y numero de categoria
         if (selection.lenght!=3){
             return selection
         }
+        good++
     }
+    alert("ERROR 444","Fallo de conexion con base de datos")
+    return -1
 }
