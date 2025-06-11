e765d0612e66630d86a3066337a7252ffc92b0c1
jbrianza-pio <jmbrianza@pioix.edu.ar>
2025-06-11 18:08:35 -0300

    Update Loop
    Hice:
    -Se partio el loop en dos, inicial de cambio a juego y final cuando se selecciona la opcion
    -planteamiento de funciones del loop
    -comentarios explicativos
    falta
    -terminar finalizer
    -cuestion respuesta de cada funcion

--- front/script.js
+++ front/script.js
@@ -16,24 +16,54 @@ function selectRandomPeli(category){
     alert("ERROR 444","Fallo de conexion con base de datos")
     return -1
 }
-function replaceSec1(){
 
-}
-function replaceSec2(){
-
-}
-function loop(category){
+// elige y pone las peliculas iniciales
+function seleccionIncial(category){
     let peliculaSec1 =selectRandomPeli(category);
-    replaceSec1(); //falta definir variable y UI
+    replaceSec1(peliculaSec1); //falta definir variable y UI // DOM
     let peliculaSec2 =selectRandomPeli(category);
-    replaceSec1(); //falta definir variable y UI
-    let game=true
+    replaceSec2(peliculaSec2); //falta definir variable y UI // DOM
+    return peliculaSec1, peliculaSec2    //falta devolver bien  valores
+}
+
+//compara cuales mayor y guarda la variable
+function compare(category,peliculaSec1,peliculaSec2){
     let mayor
-    while (game){
-        if (peliculaSec1.category>peliculaSec2.category){//compara cuales mayor
-            mayor=peliculaSec1
-        }else{
-            mayor=peliculaSec1
-        }
+    if (peliculaSec1.category>peliculaSec2.category){
+        mayor=peliculaSec1
+    }if (peliculaSec2.category>peliculaSec1.category){
+        mayor=peliculaSec1
+    }else{
+        mayor="igual"
     }
+    return mayor
+}
+
+//prepara las variables para el juego luego de la seleccion
+function changeGame(){
+    let category = getCategory()//falta definir variable y UI // DOM
+    seleccionIncial(category)
+    let corretcAnswer = compare(category,peliculaSec1,peliculaSec2)
+}
+
+//inicia la finalizacion luego de elegir
+function buttonAnswer(selecctionAnswer){//hacer llegar el atributo del boton
+    if (selecctionAnswer==corretcAnswer || selecctionAnswer=="igual") {
+        replaceSelection()
+    }else{
+        finalizer()
+    }
+}
+
+//termina y vuelve a pantalla inicial
+function finalizer(){
+
+}
+
+//cambia de posicion y busca nueva peli
+function replaceSelection(category){
+    peliculaSec1 = peliculaSec2
+    peliculaSec2 = selectRandomPeli(category);
+    replaceSec2(peliculaSec2); //falta definir variable y UI // DOM
+    let corretcAnswer = compare(category,peliculaSec1,peliculaSec2)
 }
