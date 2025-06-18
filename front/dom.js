2d6b0428a9a04c79afb0c273afb1de483a9159ff
jbrianza-pio <jmbrianza@pioix.edu.ar>
2025-06-17 21:19:07 -0300

    MERGE A MANO
    Sobreescribi los archivos modificados, por no entendimiento de problema en merge. Aparte aparecia cuestiones de que se iban a borrar archivos sino. Pensado e ideado junto a mi compañero Matias Mahlknecht

--- front/dom.js
+++ front/dom.js
@@ -1,3 +1,5 @@
+//DOM generales
+
 function getUser(){
     user = document.getElementById("inputUser").value;
     return user
@@ -6,12 +8,72 @@ function getPassword(){
     password = document.getElementById("inputPassword").value;
     return password
 }
-function popUp(){
 
-}
-function closePopUp(){
+// dom modals
 
+//const modalid = document.getElementById("modal-name")//encuentra con el modalid el elemento //Accder al elemento
+//const modalid = document.getElementById("modal-name")//encuentra con el modalid el elemento//Accder al elemento
+//const modalid = document.getElementById("modal-name")//encuentra con el modalid el elemento//Accder al elemento
+//const modalid = document.getElementById("modal-name")//encuentra con el modalid el elemento//Accder al elemento
+//const modalid = document.getElementById("modal-name")//encuentra con el modalid el elemento//Accder al elemento
+
+//modal cuenta1
+
+function showModalCuenta1(){ //agregarle el nombre
+    //modalid.showModal()
 }
+function closeModalCuenta1(){//agregarle el nombre
+    //modalid.close()
+}
+
+
+//modal cuenta2
+function showModalCuenta2(){ //agregarle el nombre
+    //modalid.showModal()
+}
+function closeModalCuenta2(){//agregarle el nombre
+    //modalid.close()
+}
+
+
+//modal ranking
+function showModalRanking(){ //agregarle el nombre
+    //modalid.showModal()
+}
+function closeModalRanking(){//agregarle el nombre
+    //modalid.close()
+}
+
+
+//modal jugar
+function showModalPlay(){ //agregarle el nombre
+    //modalid.showModal()
+}
+function closeModalPlay(){//agregarle el nombre
+    //modalid.close()
+}
+
+
+
+//modal Final
+function replaceModalFinal(maxPoint,point){
+    //igualar las diferentes partes del modal a las partes del array
+}
+function showModalFinal(){ //agregarle el nombre
+    //modalid.showModal()
+}
+function closeModalFinal(){//agregarle el nombre
+    //modalid.close()
+}
+
+// dom index
 function changeScreen(){
-    
+    //investigar
+}
+
+function replaceSec1(peliculaSec1){
+    //igualar las diferentes partes de la seccion a las partes del array
+}
+function replaceSec2(peliculaSec2){
+        //igualar las diferentes partes de la seccion a las partes del array
 }
